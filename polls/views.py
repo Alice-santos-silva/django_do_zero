6113fe3320c7c3fbe274484ea9549559b519ee83
brice de la serviere <alicesantossilva345@gmail.com>
2024-10-30 14:21:17 -0300

    template html teste

--- polls/views.py
+++ polls/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
 
-# Create your views here.
+from django.http import HttpResponse
+
+# define uma view baseada em função
+def index(request):
+    # return HttpResponse('olá django - index')
+    return render(request, 'index.html')
+
+# define outra view baseada em função
+def ola(request):
+    return HttpResponse('olá django')
