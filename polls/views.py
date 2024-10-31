1d2a76e28b25536c58dc2c5e7d4e6d31fbb292a8
brice de la serviere <alicesantossilva345@gmail.com>
2024-10-31 13:02:41 -0300

    primeiro template

--- polls/views.py
+++ polls/views.py
@@ -5,7 +5,8 @@ from django.http import HttpResponse
 # define uma view baseada em função
 def index(request):
     # return HttpResponse('olá django - index')
-    return render(request, 'index.html')
+    # return render(request, 'index.html')
+    return render(request, 'index.html', {'titulo': 'últimas enquetes'})
 
 # define outra view baseada em função
 def ola(request):
